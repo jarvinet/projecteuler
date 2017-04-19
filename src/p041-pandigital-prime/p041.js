53163e93016f6e6c0c033bf8d2051be73b0b146c
Tapani Jarvinen <tapani.jarvinen@iki.fi>
2017-04-19 10:48:43 +1000

    solution to problem 41

---
+++ src/p041-pandigital-prime/p041.js
@@ -0,0 +1,75 @@
+// This version has the fewest lines of code - but is very slow.
+// It checks if n is dividible by every integer 2, 3, 4, 5 ... 
+// up to sqrt(n)
+
+function isPrime1(n) {
+    if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
+    var m=Math.sqrt(n);
+    for (var i=2;i<=m;i++) if (n%i==0) return false;
+    return true;
+}
+
+// The next version is better: it checks the divisor 2 separately;
+// then, it proceeds to check odd divisors only, from 3 up to sqrt(n).
+// At most half of the numbers between 3 and sqrt(n) are checked.
+
+function isPrime2(n) {
+    if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
+    if (n%2==0) return (n==2);
+    var m=Math.sqrt(n);
+    for (var i=3;i<=m;i+=2) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+// Even better: first, check if n is divisible by 2 or 3.
+// Then check only the odd divisors that are not multiples of 3.
+// At most 1/3 of divisors under sqrt(n) are checked;
+// other divisors are multiples of either 2 or 3 anyway.
+
+function isPrime3(n) {
+    if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
+    if (n%2==0) return (n==2);
+    if (n%3==0) return (n==3);
+    var m=Math.sqrt(n);
+    for (var i=5;i<=m;i+=6) {
+        if (n%i==0)     return false;
+        if (n%(i+2)==0) return false;
+    }
+    return true;
+}
+
+function permutator(inputArr) {
+    var results = [];
+    
+    function permute(arr, memo) {
+        var cur, memo = memo || [];
+        
+        for (var i = 0; i < arr.length; i++) {
+            cur = arr.splice(i, 1);
+            if (arr.length === 0) {
+                results.push(memo.concat(cur));
+            }
+            permute(arr.slice(), memo.concat(cur));
+            arr.splice(i, 0, cur[0]);
+        }
+        
+        return results;
+    }
+    
+    return permute(inputArr);
+}
+
+var permutations = permutator([7,6,5,4,3,2,1]);
+
+permutations.forEach(function(permutation) {
+//    console.log("permutation", permutation);
+    var numberAsString = permutation.reduce(function(accumulator, currentValue, currentIndex, array) {
+        return accumulator + currentValue;
+    }, "");
+//    console.log("numberAsString", numberAsString);
+    var numberAsInt = new Number(numberAsString);
+//    console.log("numberAsInt", numberAsInt);
+    console.log("isPrime", isPrime2(numberAsInt), numberAsInt);
+});
